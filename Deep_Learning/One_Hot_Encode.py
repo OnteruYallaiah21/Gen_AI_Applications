7469f1624f02c9e93432b0ae50cf4cbf62473516
Yallaiah Onteru <101083258+OnteruYallaiah21@users.noreply.github.com>
2024-11-15 16:15:18 -0600

    Bag of Words and n grams

---
+++ Deep_Learning/One_Hot_Encode.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+
+Data={
+      'rollNumbers':[1,2,3,4],
+      'Names':['Yallaiah','Khadar','Rayudu','Mahesh']
+      }
+print(Data)
+df=pd.DataFrame(Data)
+print(f"originl Data frame is \n{df}")
+#encodes intilization
+encode=OneHotEncoder(sparse_output=False)
+NamesEncodeing=encode.fit_transform(df[['Names']])
+print(f"the Names are encodes is =>{NamesEncodeing}")
+Names_encoding=pd.DataFrame(NamesEncodeing,columns=encode.categories_[0])
+print(f"the dataframe in encode form is \n{Names_encoding}")
---
+++ Deep_Learning/Reduced_Spam.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+import re#support for regular expresdsion
+pStemmer=PorterStemmer()
+dfMessage=pd.read_csv('D:\Projects\LangchainProjects\DataSets\Reduced_Spam.csv')
+dfMessage=dfMessage.drop(dfMessage.columns[0],axis=1)#droping the column 
+dfMessage=dfMessage.drop(dfMessage.columns[2],axis=1)#droping the column
+dfMessage=dfMessage.rename(columns={'text':'Message'},inplace=False)
+print(dfMessage)
+#cleaning the data set and preprocessing
+corpos=[]
+for i in range (0,len(dfMessage)):
+    review=re.sub('[^a-zA-Z]',' ',dfMessage['Message'][i])#[^...] =match anything that is NOT
+    review=review.split()#converts string to list
+    
+    review=[pStemmer.stem(word) for word in review if not word in stopwords.words('english')]# after removing the stop words from the sentenses the stemming process would be applied i.e root word
+    review=' '.join(review)
+    corpos.append(corpos)
+#print(corpos[1])
+#create a bag of words model
+from sklearn.feature_extraction.text import CountVectorizer
+cv=CountVectorizer(max_features=2500,binary=True)
+cv_Transform=cv.fit_transform(corpos)
+print(corpos)
